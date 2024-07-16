14efe41022ab6e2f046a9a9aa28cee8ea54cea8d
Samuel Noronha Ramos <sam.ukasnr@gmail.com>
2024-07-16 19:50:37 -0300

    Inicial

---
+++ src/dao/Conexao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+        
+public class Conexao {
+    
+ //   public Connection getConnection(){
+    //    Connection conexao = DriverManager.getConnection("jdbc:postgreesql:");
+  //  }
+}
